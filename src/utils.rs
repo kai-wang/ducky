15bea8b613f3689a19c4804d3901ac92102bcd6a
Kai <simaow@outlook.com>
2022-10-19 22:35:24 +1100

    init commit

---
+++ src/utils.rs
@@ -0,0 +1,61 @@
+pub(crate) fn extract_digits(s: &str) -> (&str, &str) {
+    let digits_end = s
+        .char_indices()
+        .find_map(|(idx, c)| if c.is_ascii_digit() { None } else { Some(idx) } )
+        .unwrap_or_else(|| s.len());
+
+    let digits = &s[..digits_end];
+    let remaining = &s[digits_end..];
+
+    (remaining, digits)
+}
+
+pub(crate) fn extract_op(s: &str) -> (&str, &str) {
+    match &s[0..1] {
+        "+" | "-" | "*" | "/" => {}
+        _ => panic!("bad operator"),
+    }
+
+    (&s[1..], &s[0..1])
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_extract_one_digit() {
+        assert_eq!(extract_digits("1+2"), ("+2", "1"));
+        assert_eq!(extract_digits("100+200"), ("+200", "100"));
+    }
+
+    #[test]
+    fn do_not_extract_anything_from_empty_input() {
+        assert_eq!(extract_digits(""), ("", ""));
+    }
+
+    #[test]
+    fn extract_digits_without_remaning() {
+        assert_eq!(extract_digits("100"), ("", "100"));
+    }
+
+    #[test]
+    fn extract_plus() {
+        assert_eq!(extract_op("+2"), ("2", "+"));
+    }
+
+    #[test]
+    fn extract_minus() {
+        assert_eq!(extract_op("-10"), ("10", "-"));
+    }
+
+    #[test]
+    fn extract_star() {
+        assert_eq!(extract_op("*3"), ("3", "*"));
+    }
+
+    #[test]
+    fn extract_slash() {
+        assert_eq!(extract_op("/4"), ("4", "/"));
+    }
+}
