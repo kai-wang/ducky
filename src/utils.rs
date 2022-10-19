72cf6bda943e0500d9189e4e1fe3c081d411e156
Kai <simaow@outlook.com>
2022-10-19 22:57:56 +1100

    refactor

--- src/utils.rs
+++ src/utils.rs
@@ -1,13 +1,6 @@
+#[inline]
 pub(crate) fn extract_digits(s: &str) -> (&str, &str) {
-    let digits_end = s
-        .char_indices()
-        .find_map(|(idx, c)| if c.is_ascii_digit() { None } else { Some(idx) } )
-        .unwrap_or_else(|| s.len());
-
-    let digits = &s[..digits_end];
-    let remaining = &s[digits_end..];
-
-    (remaining, digits)
+    take_while(s, |c| c.is_ascii_digit())
 }
 
 pub(crate) fn extract_op(s: &str) -> (&str, &str) {
@@ -17,6 +10,22 @@ pub(crate) fn extract_op(s: &str) -> (&str, &str) {
     }
 
     (&s[1..], &s[0..1])
+}
+
+#[inline]
+pub(crate) fn extract_whitespaces(s: &str) -> (&str, &str) {
+    take_while(s, |c| c.is_whitespace())
+}
+
+pub(crate) fn take_while<F>(s: &str, f: F) -> (&str, &str)
+where F: Fn(char) -> bool
+{
+    let match_end = s
+        .char_indices()
+        .find_map(|(idx, c)| if f(c) { None } else { Some(idx) } )
+        .unwrap_or_else(|| s.len());
+
+    (&s[match_end..], &s[..match_end])
 }
 
 #[cfg(test)]
@@ -57,5 +66,10 @@ mod tests {
     #[test]
     fn extract_slash() {
         assert_eq!(extract_op("/4"), ("4", "/"));
+    }
+
+    #[test]
+    fn extract_ws() {
+        assert_eq!(extract_whitespaces("    12312"), ("12312", "    "));
     }
 }
