e775219f1dcff853dbdbca66d7c1bdee4902ae33
Kai <simaow@outlook.com>
2022-10-24 16:28:23 +1100

    refactor add source

--- crates/ducky/src/parser.rs
+++ crates/ducky/src/parser.rs
@@ -1,6 +1,7 @@
 mod expr;
 mod event;
 mod sink;
+mod source;
 
 use crate::lexer::{Lexeme, Lexer, SyntaxKind};
 use rowan::{GreenNode};
@@ -8,18 +9,17 @@ use crate::syntax::{SyntaxNode};
 use expr::expr;
 use event::Event;
 use sink::Sink;
+use source::Source;
 
 struct Parser<'l, 'input> {
-    lexemes: &'l [Lexeme<'input>],
-    cursor: usize,
+    source: Source<'l, 'input>,
     events: Vec<Event>
 }
 
 impl<'l, 'input> Parser<'l, 'input> {
     fn new(lexemes: &'l [Lexeme<'input>]) -> Self {
         Self {
-            lexemes,
-            cursor: 0,
+            source: Source::new(lexemes),
             events: Vec::new(),
         }
     }
@@ -33,28 +33,15 @@ impl<'l, 'input> Parser<'l, 'input> {
     }
 
     fn peek(&mut self) -> Option<SyntaxKind> {
-        self.eat_whitespace();
-        self.lexemes.get(self.cursor).map(|Lexeme {kind, ..}| *kind)
-    }
-
-    fn eat_whitespace(&mut self) {
-        while self.peek_raw() == Some(SyntaxKind::Whitespace) {
-            self.cursor += 1;
-        }
-    }
-
-    fn peek_raw(&self) -> Option<SyntaxKind> {
-        self.lexemes.get(self.cursor).map(|Lexeme { kind, ..}| *kind)
+        self.source.peek_kind()
     }
 
     fn bump(&mut self) {
-        self.eat_whitespace();
-        let Lexeme { kind, text } = self.lexemes[self.cursor];
-        self.cursor += 1;
+        let Lexeme { kind, text } = self.source.next_lexeme().unwrap();
         
         self.events.push(Event::AddToken { 
-            kind, 
-            text: text.into() 
+            kind: *kind, 
+            text: (*text).into() 
         });
     }
 
